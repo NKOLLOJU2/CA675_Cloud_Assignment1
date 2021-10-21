b8fad8b911c6d7edbce502e81030060067fda1e8
NKOLLOJU2 <91561274+NKOLLOJU2@users.noreply.github.com>
2021-10-21 23:47:38 +0100

    Create Step 4 Hive_Commands_for_Database_Table_Creation.sql

---
+++ Code_Pack/Step 4 Hive_Commands_for_Database_Table_Creation.sql
@@ -0,0 +1,8 @@
+--1.	To create a Database.
+	CREATE DATABASE IF NOT EXISTS cloudtechdb;
+
+--2.	To create table in the database.
+	CREATE TABLE cloudtechdb.poststb (Body string, Score int, Id int, ViewCount int, OwnerUserId int, OwnerDisplayName string, Title string, Tags string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
+
+--3.	Load the clean data from the HDFS cluster to the table
+	LOAD DATA INPATH 'hdfs://cluster-0489-nikhil-m/FinalCleanData8' INTO TABLE cloudtechdb.poststb;
