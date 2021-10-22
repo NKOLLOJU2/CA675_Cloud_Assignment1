2d55afd67d78b403a2aa9cdd70c9729ecfaf9ec2
NKOLLOJU2 <91561274+NKOLLOJU2@users.noreply.github.com>
2021-10-22 16:49:39 +0100

    Update Step 4 Hive_Commands_for_Database_Table_Creation.sql

--- Code_Pack/Step 4 Hive_Commands_for_Database_Table_Creation.sql
+++ Code_Pack/Step 4 Hive_Commands_for_Database_Table_Creation.sql
@@ -2,7 +2,7 @@
 	CREATE DATABASE IF NOT EXISTS cloudtechdb;
 
 --2.	To create table in the database.
-	CREATE TABLE cloudtechdb.top2gpoststb (Body string, Score int, Id int, ViewCount int, OwnerUserId int, OwnerDisplayName string, Title string, Tags string) ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
+	CREATE TABLE cloudtechdb.top2gpoststb (Id int,PostTypeId int,AcceptedAnswerId int,ParentId int,CreationDate timestamp,DeletionDate timestamp,Score int,ViewCount int,Body string,OwnerUserId int,OwnerDisplayName string,LastEditorUserId int,LastEditorDisplayName string,LastEditDate timestamp,LastActivityDate timestamp,Title string,Tags string,AnswerCount int,CommentCount int,FavoriteCount int,ClosedDate timestamp,CommunityOwnedDate timestamp,ContentLicense string)  ROW FORMAT DELIMITED FIELDS TERMINATED BY ',';
 
 --3.	Load the clean data from the HDFS cluster to the table
 	LOAD DATA INPATH 'hdfs://cluster-0489-nikhil-m/FinalCleanedData' INTO TABLE cloudtechdb.top2gpoststb;
