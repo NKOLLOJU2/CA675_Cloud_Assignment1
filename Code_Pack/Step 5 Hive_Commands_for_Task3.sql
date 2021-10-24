174b7770d7269b01adc9988bd70dd5b8d5a4629a
NKOLLOJU2 <91561274+NKOLLOJU2@users.noreply.github.com>
2021-10-24 23:10:10 +0100

    Update Step 5 Hive_Commands_for_Task3.sql

--- Code_Pack/Step 5 Hive_Commands_for_Task3.sql
+++ Code_Pack/Step 5 Hive_Commands_for_Task3.sql
@@ -1,12 +1,12 @@
 --1. 	The top 10 posts by score
 
-	select Title, Score from cloudtechdb.top2gpoststb ORDER BY Score DESC LIMIT 10;
+	SELECT Title, Score AS Post_Score FROM cloudtechdb.top2gpoststb ORDER BY Post_Score DESC LIMIT 10;
 	
 --2.	The top 10 users by post score
 
-	SELECT OwnerUserId AS Owner, SUM(Score) AS Grand_Score FROM cloudtechdb.top2gpoststb GROUP BY  OwnerUserId ORDER BY Grand_Score DESC LIMIT 10;
+	SELECT SUM(Score) AS Grand_Post_Score, OwnerUserId AS Owner FROM cloudtechdb.top2gpoststb GROUP BY  OwnerUserId ORDER BY Grand_Post_Score DESC LIMIT 10;
 	
---3. 	The number of distinct users, who used the word “cloud” in one of their posts
+--3. 	The number of distinct users, who used the whole word “ cloud ” in one of their posts
 
-	SELECT COUNT(DISTINCT OwnerUserId) AS Owner_Count FROM cloudtechdb.top2gpoststb WHERE (UPPER(Title) LIKE '% CLOUD %' OR UPPER(Body) LIKE '% CLOUD %' OR UPPER(Tags) LIKE '% CLOUD %');
+	SELECT COUNT ( DISTINCT OwnerUserId ) AS Owner_Count FROM cloudtechdb.top2gpoststb WHERE (UPPER(Title) LIKE '% CLOUD %' OR UPPER(body) LIKE '% CLOUD %' OR UPPER(Tags) LIKE '% CLOUD %');
 		
