e4a85849cc587ba93017e918a2f281daccc3a31a
NKOLLOJU2 <91561274+NKOLLOJU2@users.noreply.github.com>
2021-10-25 23:09:00 +0100

    y

--- Code_Pack/Step 6 Hive_Commands_for_Task4.sql
+++ Code_Pack/Step 6 Hive_Commands_for_Task4.sql
@@ -16,13 +16,13 @@ CREATE TEMPORARY MACRO max2(x INT, y INT) if(x>y,x,y);
 CREATE TEMPORARY MACRO tfidf(tf FLOAT, df_t INT, n_docs INT) tf * (log(10, CAST(n_docs as FLOAT)/max2(1,df_t)) + 1.0);
  
 --To Top 10 users sorted by their score
-CREATE TABLE cloudtechdb.task4_top10_tb1 AS SELECT OwnerUserId, Body, Score FROM cloudtechdb.top2gpoststb ORDER BY Score DESC LIMIT 10;
+CREATE TABLE cloudtechdb.task4_top10tb1 AS SELECT SUM(Score) AS Grand_Post_Score, OwnerUserId AS Owner FROM cloudtechdb.top2gpoststb GROUP BY  OwnerUserId ORDER BY Grand_Post_Score DESC LIMIT 10;
  
 --To store the OwnerUserId and Body from the previous table.
-CREATE TABLE cloudtechdb.task4_top10_tb2 AS SELECT OwnerUserId, Body FROM cloudtechdb.task4_top10_tb1;
+CREATE TABLE cloudtechdb.task4_top10tb2 AS SELECT OwnerUserId, Body FROM cloudtechdb.top2gpoststb A WHERE EXISTS (SELECT 1 FROM cloudtechdb.task4_top10tb1 B WHERE A.OwnerUserId=B.Owner);
  
 --To remove the stop words
-CREATE OR REPLACE VIEW cloudtechdb.task4_top10_vw AS SELECT OwnerUserId, de_word FROM cloudtechdb.task4_top10_tb2 LATERAL VIEW explode(split(Body, ' ')) t AS de_word WHERE NOT is_stopword(de_word);
+CREATE OR REPLACE VIEW cloudtechdb.task4_top10_vw AS SELECT OwnerUserId, de_word FROM cloudtechdb.task4_top10tb2 LATERAL VIEW explode(split(Body, ' ')) t AS de_word WHERE NOT is_stopword(de_word);
  
 --Term Frequency (TF)
 CREATE OR REPLACE VIEW cloudtechdb.task4_termfreq_vw AS SELECT OwnerUserId, de_word, freq FROM (SELECT OwnerUserId, tf(de_word) AS ea_word_freq FROM cloudtechdb.task4_top10_vw GROUP BY OwnerUserId) t LATERAL VIEW explode(ea_word_freq) t2 AS de_word, freq;
@@ -31,7 +31,7 @@ CREATE OR REPLACE VIEW cloudtechdb.task4_termfreq_vw AS SELECT OwnerUserId, de_w
 CREATE OR REPLACE VIEW cloudtechdb.task4_docfreq_vw AS SELECT de_word, COUNT(DISTINCT OwnerUserId) docs FROM cloudtechdb.task4_top10_vw GROUP BY de_word;
  
 --TF-IDF
-CREATE OR REPLACE VIEW cloudtechdb.task4_tfidf_vw AS SELECT tf.OwnerUserId, tf.de_word, wf_tfidf(tf.freq, df.docs, ${n_docs}) AS wf_tfidf FROM cloudtechdb.task4_termfreq_vw tf JOIN cloudtechdb.task4_docfreq_vw df ON (tf.de_word = df.de_word) ORDER BY wf_tfidf DESC;
+CREATE OR REPLACE VIEW cloudtechdb.task4_tfidf_vw AS SELECT tf.OwnerUserId, tf.de_word, tfidf(tf.freq, df.docs, ${n_docs}) AS wf_tfidf FROM cloudtechdb.task4_termfreq_vw tf JOIN cloudtechdb.task4_docfreq_vw df ON (tf.de_word = df.de_word) ORDER BY wf_tfidf DESC;
 
 --Calculate the per-user TF-IDF of the top 10 terms for each of the top 10 users
-SELECT OwnerUserId ,de_word, wf_tfidf, rank FROM ( SELECT  OwnerUserId ,de_word, wf_tfidf, ROW_NUMBER() over (PARTITION BY OwnerUserId ORDER BY wf_tfidf DESC) as rank FROM cloudtechdb.task4_tfidf_vw  ) A  WHERE rank <= 10;
+SELECT OwnerUserId ,de_word, wf_tfidf, rank FROM ( SELECT  OwnerUserId ,de_word, wf_tfidf, ROW_NUMBER() over (PARTITION BY OwnerUserId ORDER BY wf_tfidf DESC) as rank FROM cloudtechdb.task4_tfidf_vw ) A  WHERE rank <= 10;
